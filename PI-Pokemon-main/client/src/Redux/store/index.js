92530f06160d6ae5dd2851a128bee459f6afb478
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-06 19:40:09 -0300

    .

--- PI-Pokemon-main/client/src/Redux/store/index.js
+++ PI-Pokemon-main/client/src/Redux/store/index.js
@@ -1,7 +1,9 @@
-import rootReducer from "../reducers";
-import {createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "../reducers";
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
