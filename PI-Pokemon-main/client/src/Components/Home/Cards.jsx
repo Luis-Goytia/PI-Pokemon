25783769f841fb401882c0f856b6696fdca5adfa
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-06 19:43:28 -0300

    .

---
+++ PI-Pokemon-main/client/src/Components/Home/Cards.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "../Card/Card";
+import './Home.css'
+
+
+
+export default function Cards({ data }) {
+
+  return(
+    <div className="container">
+    {data?.map((el) => (
+      <div key={el.id}>
+        <Card
+          id={el.id}
+          name={el.name}
+          types={el.types}
+          image={el.img}
+        />
+      </div>
+    ))}
+  </div>
+  )
+}
