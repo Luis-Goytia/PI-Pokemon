fee26bab3a630b8fe92334ae4b70198a7e8b912d
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-08 10:47:57 -0300

    .

--- PI-Pokemon-main/client/src/Components/LandingPage/LandingPage.jsx
+++ PI-Pokemon-main/client/src/Components/LandingPage/LandingPage.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
-import logo from "../../resources/Images/pokemon.png";
-import charizard from "../../resources/no-pokemon.362d9a00.gif";
+
 
 export default function LandingPage() {
   return (
