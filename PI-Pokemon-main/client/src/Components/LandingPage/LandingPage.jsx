f619fab6a05f58279b6aa111c5b7a8eb5d133419
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-08 11:10:31 -0300

    .

--- PI-Pokemon-main/client/src/Components/LandingPage/LandingPage.jsx
+++ PI-Pokemon-main/client/src/Components/LandingPage/LandingPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
-
-
+import logo from "../../resources/Images/pokemon.png";
 export default function LandingPage() {
   return (
     <div className={styles.background}>
-      
-          <Link to="/home">
-            <button className={styles.button}>Let's go !</button>
+      <div>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <div>
+          <h1 className={styles.title}>¡Welcome to your pokedex!</h1>
+          <Link to={"/home"}>
+            <button className={styles.button}>
+              <span>Let's go !</span>
+            </button>
           </Link>
-        
+        </div>
+      </div>
     </div>
   );
 }
